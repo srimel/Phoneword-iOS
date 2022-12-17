2b68969105b837a4ae4e94eb4a51bab95867ee29
Stuart Rimel <stuart.rimel@gmail.com>
2022-12-17 13:50:22 -0800

    initial commit

---
+++ Phoneword2/ViewController.designer.cs
@@ -0,0 +1,50 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Phoneword2
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton CallButton { get; set; }
+
+		[Outlet]
+		UIKit.UITextField PhoneNumberText { get; set; }
+
+		[Outlet]
+		UIKit.UILabel PhonewordLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIButton TranslateButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (CallButton != null) {
+				CallButton.Dispose ();
+				CallButton = null;
+			}
+
+			if (PhoneNumberText != null) {
+				PhoneNumberText.Dispose ();
+				PhoneNumberText = null;
+			}
+
+			if (PhonewordLabel != null) {
+				PhonewordLabel.Dispose ();
+				PhonewordLabel = null;
+			}
+
+			if (TranslateButton != null) {
+				TranslateButton.Dispose ();
+				TranslateButton = null;
+			}
+		}
+	}
+}
